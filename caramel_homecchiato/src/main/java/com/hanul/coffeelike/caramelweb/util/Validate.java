9dd460a729fdac9251336381b139829c81b0c6ee
iwasamistake <iwasamistake4@gmail.com>
2020-12-27 19:37:22 +0900

    Regex 업데이트

--- caramel_homecchiato/src/main/java/com/hanul/coffeelike/caramelweb/util/Validate.java
+++ caramel_homecchiato/src/main/java/com/hanul/coffeelike/caramelweb/util/Validate.java
@@ -5,13 +5,13 @@ import java.util.regex.Pattern;
 public final class Validate{
 	private Validate(){}
 
-	private static final Pattern EMAIL_NAME = Pattern.compile("[^\t\n]{1,25}");
+	private static final Pattern NAME_REGEX = Pattern.compile("[^\t\n]{1,40}");
 	private static final Pattern EMAIL_REGEX = Pattern.compile("[^@]+@[^@]+");
 	private static final Pattern PHONE_NUMBER_REGEX = Pattern.compile("\\d{3}[ -]?\\d{4}[ -]?\\d{4}");
 	private static final Pattern PASSWORD_REGEX = Pattern.compile("\\S{3,63}");
 
 	public static boolean name(String name){
-		return EMAIL_NAME.matcher(name).matches();
+		return NAME_REGEX.matcher(name).matches();
 	}
 	public static boolean email(String email){
 		return EMAIL_REGEX.matcher(email).matches();
